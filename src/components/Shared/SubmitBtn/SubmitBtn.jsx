d5bb03ce58219fff568e7776e2c6f71ae9217d0c
lit <lit@danskebank.lt>
2021-09-13 19:49:54 +0300

    button_alert

--- src/components/Shared/SubmitBtn/SubmitBtn.jsx
+++ src/components/Shared/SubmitBtn/SubmitBtn.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import css from "./SubmitBtn.module.css";
 
 function SubmitBtn({isHidden}) {
+  const clickHandler = () =>{
+    alert('Further step is in development.')
+  }
   return (
     <div>
-      <button disabled={isHidden} className={css.button}>Se boliger</button>
+      <button disabled={isHidden} onClick={clickHandler} className={css.button}>Se boliger</button>
     </div>
   );
 }
